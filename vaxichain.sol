5dacc14731fdfd6b23e228bd873d3422f85cbffd
JITHO V JOY <jithovjoy@cet.ac.in>
2021-05-29 18:39:45 +0530

    Create vaxichain.sol

---
+++ vaxichain.sol
@@ -0,0 +1,56 @@
+pragma solidity ^0.6.2;
+contract TrustChainVersion1{
+    mapping(uint => Request) public request;
+    mapping(uint => Verification) public verify;
+    uint256 public requestCount = 0;
+    struct Request{
+        address payable author;
+        string requestHash;
+        uint256 amount;
+        bool fullfilled;
+    }
+    struct Verification{
+        uint id;
+        uint256 collectedAmount;
+        uint valid;
+
+    }
+    event RequestCreated(
+        address payable author,
+        string requestHash,
+        uint256 amount,
+        bool fullfilled
+    );
+
+    event Verified(
+        uint id,
+        uint256 collectedAmount,
+        uint valid
+    );
+
+
+
+    function makeRequest(string memory _rHash, uint256 _amount) public{
+        requestCount++;
+        request[requestCount] = Request(msg.sender, _rHash, _amount, false);
+        verify[requestCount] = Verification(requestCount, 0, 0);
+        emit RequestCreated(msg.sender, _rHash, _amount, false);
+    }
+
+
+
+    function sendMoney(uint _id, uint256 _money )public{
+        verify[_id].collectedAmount += _money;
+        verify[_id].valid += 1;
+        uint256 amt = request[_id].amount - verify[_id].collectedAmount;
+        if(amt == 0){
+            request[_id].fullfilled = true;
+        }
+        else if(amt < 0){
+            request[_id].amount = 0;
+            request[_id].fullfilled = true;
+
+        }
+        emit Verified(_id, _money, 1);
+        }
+  }
