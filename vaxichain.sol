70c1fb6392b39cdb9ea39c1f6b5e6f10a23f0ef7
JITHO V JOY <jithovjoy@cet.ac.in>
2021-06-16 21:57:03 +0530

    Rules, violation, sideEffects tracking etc..

--- vaxichain.sol
+++ vaxichain.sol
@@ -7,6 +7,8 @@ contract VaxiChain{
     mapping(uint => Distributor) public distributor;
     mapping(uint => VaccineCenter) public vaccineCenter;
     mapping(uint => Beneficiary) public beneficiary;
+    mapping(string => Rules ) public rules;
+    mapping(string => Violation ) public violation;
     
     uint256 public manufactureCount = 0;
     uint256 public distributorCount = 0;
@@ -20,14 +22,39 @@ contract VaxiChain{
         string location;
        
     }
+    
+    struct Rules{
+        string vaccine_id;
+        uint min_temp;
+        uint max_temp;
+        uint doses;
+        string manuactureDate;
+        
+    }
+    
+    struct Violation{
+        string vaccine_id;
+        bool violated;
+    }
+    
+    struct SideEffects{
+        string vaccine_id;
+        uint beneficiary_id;
+        string sideEffect;
+    }
+    
     struct Vaccine{
         string vaccine_id;
         string name;
-        uint manufactureID;
-        uint distributorID;
-        uint vaccineceterID;
-        
-   }
+        uint vails;
+        string expdate;
+        uint manufactureId;
+        string arrived_dateDistributor;
+        uint distributorId;
+        string arrived_dateVaccineCenter;
+        uint vaccineCenterId;
+    }
+    
     struct Distributor{
         string licenceNO;
         string name;
@@ -41,20 +68,22 @@ contract VaxiChain{
         string location;
 
    }
+   
     struct Doctor{
         string licenceNO;
         string name;     
         string phone;
-   }    
+   }  
+
     struct Beneficiary{
         string name;
+        string email;
         string gender;
         string adharID;
         uint doctorID;
         string vaccinated_date;
         string vaccine_center;
         bool vaccinated;
-        
     }
    
     
@@ -73,17 +102,28 @@ contract VaxiChain{
         vaccineCenter[vaccineCenterCount] = VaccineCenter(licenceNo, name, phone, location);
    }
         
-    function AddBeneficiary(string memory name, string memory gender, string memory  AdharID, uint doctorID, string memory vaccinated_date,  string memory vaccine_center, bool vaccinated ) public{
+    function AddBeneficiary(string memory name, string memory email, string memory gender, string memory  AdharID, uint doctorID, string memory vaccinated_date,  string memory vaccine_center, bool vaccinated ) public{
         beneficiaryCount++;
-        beneficiary[beneficiaryCount] = Beneficiary(name,gender, AdharID, doctorID, vaccinated_date,vaccine_center,vaccinated);
+        beneficiary[beneficiaryCount] = Beneficiary(name, email, gender, AdharID, doctorID, vaccinated_date,vaccine_center,vaccinated);
    }   
    
    function AddDoctor(string memory licenseNo, string memory name, string memory phone)public{
        doctorCount++;
        doctor[doctorCount] = Doctor(licenseNo, name, phone);
    }
+   
    function ManuFacturedVaccine(string memory id, string memory name, uint manfacturer, uint dist, uint vaccine_center )public{
        vaccine[id] = Vaccine(id, name, manfacturer, dist, vaccine_center);
    }
+   
+   function tracking(string memory vaccine_id, uint temp)public{
+       if(temp < rules[vaccine_id].max_temp && temp > rules[vaccine_id].min_temp){
+           violation[vaccine_id].violated = true;
+       }else{
+           violation[vaccine_id].violated = false;
+       }
+   }
+   
+   
         
 }
